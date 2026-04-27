14c83cdceb97cb1c5d499da3a3985e3b5052727d
bhardwajrishu04-droid <bhardwaj.rishu04@gmail.com>
2026-04-27 18:06:00 +0530

    Update risk_management.py

--- risk_management.py
+++ risk_management.py
@@ -1,2 +1,5 @@
-def position_size(capital, risk_per_trade):
+def position_size(capital, risk_per_trade, price):
+    risk_amount = capital * risk_per_trade
+    size = risk_amount / price
+    return round(size, 2)
     return capital * risk
